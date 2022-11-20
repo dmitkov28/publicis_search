42ac44f9fe20fe7a5cef64f2afac2877d39ed998
dmitkov28 <mitkov.dimitar@yahoo.com>
2022-11-20 18:26:46 +0200

    added loading spinner to entry detail page

--- src/pages/Timelines/components/EntryDetails.js
+++ src/pages/Timelines/components/EntryDetails.js
@@ -1,13 +1,8 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useFetchFromDB } from "../../../hooks/useFetchFromDB"
-import QueryCard from "../../../components/Keywords/Results/QueryCard";
-import ResultCard from "../../../components/Keywords/Results/ResultCard";
-import { Masonry } from "@mui/lab";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useTheme, useMediaQuery } from "@mui/material";
 
 
@@ -24,41 +19,13 @@ export default function EntryDetails() {
 
     return (
         <>
-        {data && JSON.stringify(data)}
-        {/* {data &&
-                        // <>
-                        //     <QueryCard />
-                        //     <Grid container sx={{ mt: 2, mx: 'auto' }} rowSpacing={3}>
-                        //         {Object.keys(data.data).map(modifier_type => {
-                        //             if (Object.values(data.data[modifier_type]).some(item => item.length > 0)) {
-                        //                 return (
-
-                        //                     <Grid key={modifier_type} item xs={12}>
-                        //                         <Paper sx={{ maxWidth: '1100px', m: 'auto', px: 3, py: 1 }} elevation={3}>
-                        //                             <Typography variant="h6" fontWeight='bold' sx={{ mb: 1 }}>{modifier_type}</Typography>
-                        //                             <Divider />
-                        //                             <Masonry columns={Object.keys(data.data[modifier_type]).length == 1 || isSm ? 1 : 3} spacing={3} sx={{ mt: 3 }}>
-                        //                                 {Object.entries(data.data[modifier_type]).map(([modifier_keyword, suggestions]) => {
-                        //                                     if (data.data[modifier_type][modifier_keyword].length > 0) {
-                        //                                         return (
-                        //                                             <ResultCard
-                        //                                                 key={`${modifier_type}_${modifier_keyword}`} modifier_type={modifier_type}
-                        //                                                 modifier_keyword={modifier_keyword}
-                        //                                                 suggestions={suggestions}
-                        //                                             />
-                        //                                         )
-                        //                                     }
-                        //                                 })}
-
-                        //                             </Masonry>
-                        //                         </Paper>
-                        //                     </Grid>
-                        //                 )
-                        //             }
-                        //         })}
-                        //     </Grid>
-                        // </>
-                    } */}
+            {
+                isFetching &&
+                <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', p: 2 }}>
+                    <CircularProgress />
+                </Box>
+            }
+            {data && JSON.stringify(data)}
         </>
     )
 }
